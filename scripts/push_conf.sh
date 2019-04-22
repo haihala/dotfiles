e317f052843bf0a235f9bea25632a82fcaf4951d
Eero Häihälä <eero.haihala@gmail.com>
2019-04-22 21:07:51 +0300

    removed add_bin, merged it to push_conf. Made failsafes to push_conf, no clue what happened to wlan_jank

--- scripts/push_conf.sh
+++ scripts/push_conf.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+binaries=(add_bin push_conf note wallpaper_update)
+
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
     DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
@@ -9,17 +11,30 @@ done
 CONFIG_ROOT="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
 CONFIG_ROOT="$(dirname "$CONFIG_ROOT")"
 
+
+cd $CONFIG_ROOT
+# Make sure all scripts are usable
+chmod -R +x scripts
+
 # All dotfiles and folders
 ln -fs $CONFIG_ROOT/dotfiles/.* $HOME
 echo "linked dotfiles"
 
-# Binaries
-ln -fs $CONFIG_ROOT/bin/* $HOME/bin
-echo "linked binaries to $HOME/bin"
-chmod -R +x $HOME/bin
-echo "made sure everything in $HOME/bin is executable"
+# Binaries (link some from scripts to ~/bin)i
+if [ ! -d $HOME/bin ]; then
+    echo "~/bin doesn't exist, creating"
+    mkdir $HOME/bin
+fi
+for s in ${binaries[*]}; do
+    echo "Linking $s to bin"
+    ln -fs $CONFIG_ROOT/scripts/$s.sh $HOME/bin/$s
+done
 
 # Home ".config"
+if [ ! -d $HOME/.config ]; then
+    echo "~/.config doesn't exist, creating"
+    mkdir $HOME/.config
+fi
 ln -fs $CONFIG_ROOT/home_config/* $HOME/.config/
 echo "linked things to $HOME/.config"
 

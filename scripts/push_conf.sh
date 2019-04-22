3c7cbc58e36293b2185b001a91d1974328469899
Eero Häihälä <eero.haihala@gmail.com>
2019-04-22 23:07:00 +0300

    made push_conf a lot prettier and added meta config where one can list what to link

--- scripts/push_conf.sh
+++ scripts/push_conf.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-binaries=(add_bin push_conf note wallpaper_update)
-
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
     DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
@@ -11,33 +9,54 @@ done
 CONFIG_ROOT="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
 CONFIG_ROOT="$(dirname "$CONFIG_ROOT")"
 
+bold=$(tput smso)
+normal=$(tput sgr0)
+color='\033[0;36m'
+nc='\033[0m'
+
+header() {
+    echo -e "\t$color$bold$1$normal$nc"
+}
 
 cd $CONFIG_ROOT
 # Make sure all scripts are usable
 chmod -R +x scripts
 
 # All dotfiles and folders
-ln -fs $CONFIG_ROOT/dotfiles/.* $HOME
-echo "linked dotfiles"
+header $HOME
+for dot in `ls -AL $CONFIG_ROOT/dotfiles/`
+do
+    ln -fs $CONFIG_ROOT/dotfiles/$dot $HOME
+    echo "linked $dot to $HOME"
+done
 
 # Binaries (link some from scripts to ~/bin)i
+header $HOME/bin
 if [ ! -d $HOME/bin ]; then
     echo "~/bin doesn't exist, creating"
     mkdir $HOME/bin
 fi
-for s in ${binaries[*]}; do
-    echo "Linking $s to bin"
+while read s; do
+    echo "linked $s to bin"
     ln -fs $CONFIG_ROOT/scripts/$s.sh $HOME/bin/$s
-done
+done <meta_config/linked_scripts
 
 # Home ".config"
+header $HOME/.config
 if [ ! -d $HOME/.config ]; then
     echo "~/.config doesn't exist, creating"
     mkdir $HOME/.config
 fi
-ln -fs $CONFIG_ROOT/home_config/* $HOME/.config/
-echo "linked things to $HOME/.config"
+
+for conf in `ls -AL $CONFIG_ROOT/home_config/`
+do
+    ln -fs $CONFIG_ROOT/home_config/$conf $HOME/.config/
+    echo "linked $conf to $HOME/.config/"
+done
 
 # Reload i3
-i3-msg -t command restart
-echo "Restarted i3"
+if [ pgrep i3 > /dev/null 2>&1 -eq 0 ]; then
+    echo "Restarting i3"
+    i3-msg -t command restart > /dev/null
+fi
+

6c117285cf79d0557da14d883d29d373ded0f50c
Eero Häihälä <eero.haihala@vincit.fi>
2021-05-12 19:08:30 +0300

    Add simple scritp for installing packages with dnf

---
+++ dnf_install.sh
@@ -0,0 +1,21 @@
+dnf install \
+	acpi \
+	alacritty \
+	chromium \
+	code \
+	git \
+	grim \
+	mako \
+	mpv \
+	neovim \
+	python3 \
+	slurp \
+	sway \
+	swaybg \
+	swayidle \
+	swaylock-effects \
+	waybar \
+	wdisplays \
+	wofi \
+	wl-clipboard \
+	zsh
